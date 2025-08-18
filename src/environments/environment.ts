ffdd2543eea14d5cfa3372dbc7998b123c61e1e5
jhonl23 <jhon.lizarbe.23@gmail.com>
2025-08-18 13:05:50 +0100

    config yml and config env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    uriAssetsImages: '/nxs-website-webapp/assets/images',
+    uriAssetsImages: '/assets/images',
     formspreeApi: 'https://formspree.io/f/xnnzbwrq'
 };

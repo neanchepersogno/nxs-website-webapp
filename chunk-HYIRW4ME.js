2b8bc5c162c45e10158021d09923e92cdf2c517f
neanchepersogno <neanchepersogno@users.noreply.github.com>
2025-08-18 11:33:45 +0000

    deploy: cdfb476b6c334dfcf82c53a476d5196a3336e11e

---
+++ chunk-HYIRW4ME.js
@@ -0,0 +1,141 @@
+import{a as $,b as j,c as Y,d as gt}from"./chunk-5DKNLUWN.js";import{A as E,C as G,E as M,F as Z,J,L as ht,M as vt,N as X,S as mt,j as bt,m as pt,n as L,q as K,x as ut,y as dt,z as ft}from"./chunk-S6JLVHSI.js";import{$ as R,$a as H,Aa as T,Gb as k,Hb as lt,Ia as S,Jb as l,Kb as ct,La as r,M as D,Ma as at,N as et,O as nt,Oa as p,Pa as ot,Qa as h,T as g,Wa as s,Xa as A,Z as V,Za as C,_ as P,aa as b,ab as w,bb as u,cb as v,db as m,gb as U,hb as Q,ja as y,jb as F,ka as it,kb as _,la as N,lb as x,mb as B,nb as z,ob as I,pb as d,qb as f,vb as st,xb as rt}from"./chunk-JSFZLMEL.js";var yt=(()=>{class t extends j{static \u0275fac=(()=>{let e;return function(n){return(e||(e=b(t)))(n||t)}})();static \u0275cmp=r({type:t,selectors:[["ChevronLeftIcon"]],features:[p],decls:2,vars:5,consts:[["width","14","height","14","viewBox","0 0 14 14","fill","none","xmlns","http://www.w3.org/2000/svg"],["d","M9.61296 13C9.50997 13.0005 9.40792 12.9804 9.3128 12.9409C9.21767 12.9014 9.13139 12.8433 9.05902 12.7701L3.83313 7.54416C3.68634 7.39718 3.60388 7.19795 3.60388 6.99022C3.60388 6.78249 3.68634 6.58325 3.83313 6.43628L9.05902 1.21039C9.20762 1.07192 9.40416 0.996539 9.60724 1.00012C9.81032 1.00371 10.0041 1.08597 10.1477 1.22959C10.2913 1.37322 10.3736 1.56698 10.3772 1.77005C10.3808 1.97313 10.3054 2.16968 10.1669 2.31827L5.49496 6.99022L10.1669 11.6622C10.3137 11.8091 10.3962 12.0084 10.3962 12.2161C10.3962 12.4238 10.3137 12.6231 10.1669 12.7701C10.0945 12.8433 10.0083 12.9014 9.91313 12.9409C9.81801 12.9804 9.71596 13.0005 9.61296 13Z","fill","currentColor"]],template:function(i,n){i&1&&(R(),u(0,"svg",0),m(1,"path",1),v()),i&2&&(H(n.getClassNames()),s("aria-label",n.ariaLabel)("aria-hidden",n.ariaHidden)("role",n.role))},encapsulation:2})}return t})();var _t=(()=>{class t extends j{static \u0275fac=(()=>{let e;return function(n){return(e||(e=b(t)))(n||t)}})();static \u0275cmp=r({type:t,selectors:[["ChevronRightIcon"]],features:[p],decls:2,vars:5,consts:[["width","14","height","14","viewBox","0 0 14 14","fill","none","xmlns","http://www.w3.org/2000/svg"],["d","M4.38708 13C4.28408 13.0005 4.18203 12.9804 4.08691 12.9409C3.99178 12.9014 3.9055 12.8433 3.83313 12.7701C3.68634 12.6231 3.60388 12.4238 3.60388 12.2161C3.60388 12.0084 3.68634 11.8091 3.83313 11.6622L8.50507 6.99022L3.83313 2.31827C3.69467 2.16968 3.61928 1.97313 3.62287 1.77005C3.62645 1.56698 3.70872 1.37322 3.85234 1.22959C3.99596 1.08597 4.18972 1.00371 4.3928 1.00012C4.59588 0.996539 4.79242 1.07192 4.94102 1.21039L10.1669 6.43628C10.3137 6.58325 10.3962 6.78249 10.3962 6.99022C10.3962 7.19795 10.3137 7.39718 10.1669 7.54416L4.94102 12.7701C4.86865 12.8433 4.78237 12.9014 4.68724 12.9409C4.59212 12.9804 4.49007 13.0005 4.38708 13Z","fill","currentColor"]],template:function(i,n){i&1&&(R(),u(0,"svg",0),m(1,"path",1),v()),i&2&&(H(n.getClassNames()),s("aria-label",n.ariaLabel)("aria-hidden",n.ariaHidden)("role",n.role))},encapsulation:2})}return t})();var Bt=["previcon"],It=["nexticon"],kt=["content"],Lt=["prevButton"],$t=["nextButton"],Dt=["inkbar"],Ft=["tabs"],O=["*"],Et=t=>({"p-tablist-viewport":t});function Mt(t,c){t&1&&U(0)}function Ot(t,c){if(t&1&&h(0,Mt,1,0,"ng-container",11),t&2){let e=_(2);A("ngTemplateOutlet",e.prevIconTemplate||e._prevIconTemplate)}}function Vt(t,c){t&1&&m(0,"ChevronLeftIcon")}function Pt(t,c){if(t&1){let e=Q();u(0,"button",10,3),F("click",function(){V(e);let n=_();return P(n.onPrevButtonClick())}),h(2,Ot,1,1,"ng-container")(3,Vt,1,0,"ChevronLeftIcon"),v()}if(t&2){let e=_();s("aria-label",e.prevButtonAriaLabel)("tabindex",e.tabindex())("data-pc-group-section","navigator"),T(2),w(e.prevIconTemplate||e._prevIconTemplate?2:3)}}function Rt(t,c){t&1&&U(0)}function Nt(t,c){if(t&1&&h(0,Rt,1,0,"ng-container",11),t&2){let e=_(2);A("ngTemplateOutlet",e.nextIconTemplate||e._nextIconTemplate)}}function St(t,c){t&1&&m(0,"ChevronRightIcon")}function At(t,c){if(t&1){let e=Q();u(0,"button",12,4),F("click",function(){V(e);let n=_();return P(n.onNextButtonClick())}),h(2,Nt,1,1,"ng-container")(3,St,1,0,"ChevronRightIcon"),v()}if(t&2){let e=_();s("aria-label",e.nextButtonAriaLabel)("tabindex",e.tabindex())("data-pc-group-section","navigator"),T(2),w(e.nextIconTemplate||e._nextIconTemplate?2:3)}}function Ht(t,c){t&1&&B(0)}var Qt=({dt:t})=>`
+.p-tabs {
+    display: flex;
+    flex-direction: column;
+}
+
+.p-tablist {
+    display: flex;
+    position: relative;
+}
+
+.p-tabs-scrollable > .p-tablist {
+    overflow: hidden;
+}
+
+.p-tablist-viewport {
+    overflow-x: auto;
+    overflow-y: hidden;
+    scroll-behavior: smooth;
+    scrollbar-width: none;
+    overscroll-behavior: contain auto;
+}
+
+.p-tablist-viewport::-webkit-scrollbar {
+    display: none;
+}
+
+.p-tablist-tab-list {
+    position: relative;
+    display: flex;
+    background: ${t("tabs.tablist.background")};
+    border-style: solid;
+    border-color: ${t("tabs.tablist.border.color")};
+    border-width: ${t("tabs.tablist.border.width")};
+}
+
+.p-tablist-content {
+    flex-grow: 1;
+}
+
+.p-tablist-nav-button {
+    all: unset;
+    position: absolute !important;
+    flex-shrink: 0;
+    top: 0;
+    z-index: 2;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: ${t("tabs.nav.button.background")};
+    color: ${t("tabs.nav.button.color")};
+    width: ${t("tabs.nav.button.width")};
+    transition: color ${t("tabs.transition.duration")}, outline-color ${t("tabs.transition.duration")}, box-shadow ${t("tabs.transition.duration")};
+    box-shadow: ${t("tabs.nav.button.shadow")};
+    outline-color: transparent;
+    cursor: pointer;
+}
+
+.p-tablist-nav-button:focus-visible {
+    z-index: 1;
+    box-shadow: ${t("tabs.nav.button.focus.ring.shadow")};
+    outline: ${t("tabs.nav.button.focus.ring.width")} ${t("tabs.nav.button.focus.ring.style")} ${t("tabs.nav.button.focus.ring.color")};
+    outline-offset: ${t("tabs.nav.button.focus.ring.offset")};
+}
+
+.p-tablist-nav-button:hover {
+    color: ${t("tabs.nav.button.hover.color")};
+}
+
+.p-tablist-prev-button {
+    left: 0;
+}
+
+.p-tablist-next-button {
+    right: 0;
+}
+
+.p-tab {
+    display: flex;
+    align-items: center;
+    flex-shrink: 0;
+    cursor: pointer;
+    user-select: none;
+    position: relative;
+    border-style: solid;
+    white-space: nowrap;
+    gap: ${t("tabs.tab.gap")};
+    background: ${t("tabs.tab.background")};
+    border-width: ${t("tabs.tab.border.width")};
+    border-color: ${t("tabs.tab.border.color")};
+    color: ${t("tabs.tab.color")};
+    padding: ${t("tabs.tab.padding")};
+    font-weight: ${t("tabs.tab.font.weight")};
+    transition: background ${t("tabs.transition.duration")}, border-color ${t("tabs.transition.duration")}, color ${t("tabs.transition.duration")}, outline-color ${t("tabs.transition.duration")}, box-shadow ${t("tabs.transition.duration")};
+    margin: ${t("tabs.tab.margin")};
+    outline-color: transparent;
+}
+
+.p-tab:not(.p-disabled):focus-visible {
+    z-index: 1;
+    box-shadow: ${t("tabs.tab.focus.ring.shadow")};
+    outline: ${t("tabs.tab.focus.ring.width")} ${t("tabs.tab.focus.ring.style")} ${t("tabs.tab.focus.ring.color")};
+    outline-offset: ${t("tabs.tab.focus.ring.offset")};
+}
+
+.p-tab:not(.p-tab-active):not(.p-disabled):hover {
+    background: ${t("tabs.tab.hover.background")};
+    border-color: ${t("tabs.tab.hover.border.color")};
+    color: ${t("tabs.tab.hover.color")};
+}
+
+.p-tab-active {
+    background: ${t("tabs.tab.active.background")};
+    border-color: ${t("tabs.tab.active.border.color")};
+    color: ${t("tabs.tab.active.color")};
+}
+
+.p-tabpanels {
+    background: ${t("tabs.tabpanel.background")};
+    color: ${t("tabs.tabpanel.color")};
+    padding: ${t("tabs.tabpanel.padding")};
+    outline: 0 none;
+}
+
+.p-tabpanel:focus-visible {
+    box-shadow: ${t("tabs.tabpanel.focus.ring.shadow")};
+    outline: ${t("tabs.tabpanel.focus.ring.width")} ${t("tabs.tabpanel.focus.ring.style")} ${t("tabs.tabpanel.focus.ring.color")};
+    outline-offset: ${t("tabs.tabpanel.focus.ring.offset")};
+}
+
+.p-tablist-active-bar {
+    z-index: 1;
+    display: block;
+    position: absolute;
+    bottom: ${t("tabs.active.bar.bottom")};
+    height: ${t("tabs.active.bar.height")};
+    background: ${t("tabs.active.bar.background")};
+    transition: 250ms cubic-bezier(0.35, 0, 0.25, 1);
+}
+`,zt={root:({props:t})=>["p-tabs p-component",{"p-tabs-scrollable":t.scrollable}]},Tt=(()=>{class t extends mt{name="tabs";theme=Qt;classes=zt;static \u0275fac=(()=>{let e;return function(n){return(e||(e=b(t)))(n||t)}})();static \u0275prov=et({token:t,factory:t.\u0275fac})}return t})();var Ct=(()=>{class t extends ${prevIconTemplate;nextIconTemplate;templates;content;prevButton;nextButton;inkbar;tabs;pcTabs=g(D(()=>q));isPrevButtonEnabled=N(!1);isNextButtonEnabled=N(!1);resizeObserver;showNavigators=l(()=>this.pcTabs.showNavigators());tabindex=l(()=>this.pcTabs.tabindex());scrollable=l(()=>this.pcTabs.scrollable());constructor(){super(),ct(()=>{this.pcTabs.value(),K(this.platformId)&&setTimeout(()=>{this.updateInkBar()})})}get prevButtonAriaLabel(){return this.config.translation.aria.previous}get nextButtonAriaLabel(){return this.config.translation.aria.next}ngAfterViewInit(){super.ngAfterViewInit(),this.showNavigators()&&K(this.platformId)&&(this.updateButtonState(),this.bindResizeObserver())}_prevIconTemplate;_nextIconTemplate;ngAfterContentInit(){this.templates.forEach(e=>{switch(e.getType()){case"previcon":this._prevIconTemplate=e.template;break;case"nexticon":this._nextIconTemplate=e.template;break}})}ngOnDestroy(){this.unbindResizeObserver(),super.ngOnDestroy()}onScroll(e){this.showNavigators()&&this.updateButtonState(),e.preventDefault()}onPrevButtonClick(){let e=this.content.nativeElement,i=M(e),n=Math.abs(e.scrollLeft)-i,a=n<=0?0:n;e.scrollLeft=Z(e)?-1*a:a}onNextButtonClick(){let e=this.content.nativeElement,i=M(e)-this.getVisibleButtonWidths(),n=e.scrollLeft+i,a=e.scrollWidth-i,o=n>=a?a:n;e.scrollLeft=Z(e)?-1*o:o}updateButtonState(){let e=this.content?.nativeElement,i=this.el?.nativeElement,{scrollWidth:n,offsetWidth:a}=e,o=Math.abs(e.scrollLeft),W=M(e);this.isPrevButtonEnabled.set(o!==0),this.isNextButtonEnabled.set(i.offsetWidth>=a&&o!==n-W)}updateInkBar(){let e=this.content?.nativeElement,i=this.inkbar?.nativeElement,n=this.tabs?.nativeElement,a=dt(e,'[data-pc-name="tab"][data-p-active="true"]');i&&(i.style.width=ut(a)+"px",i.style.left=G(a).left-G(n).left+"px")}getVisibleButtonWidths(){let e=this.prevButton?.nativeElement,i=this.nextButton?.nativeElement;return[e,i].reduce((n,a)=>a?n+M(a):n,0)}bindResizeObserver(){this.resizeObserver=new ResizeObserver(()=>this.updateButtonState()),this.resizeObserver.observe(this.el.nativeElement)}unbindResizeObserver(){this.resizeObserver&&(this.resizeObserver.unobserve(this.el.nativeElement),this.resizeObserver=null)}static \u0275fac=function(i){return new(i||t)};static \u0275cmp=r({type:t,selectors:[["p-tablist"]],contentQueries:function(i,n,a){if(i&1&&(z(a,Bt,4),z(a,It,4),z(a,vt,4)),i&2){let o;d(o=f())&&(n.prevIconTemplate=o.first),d(o=f())&&(n.nextIconTemplate=o.first),d(o=f())&&(n.templates=o)}},viewQuery:function(i,n){if(i&1&&(I(kt,5),I(Lt,5),I($t,5),I(Dt,5),I(Ft,5)),i&2){let a;d(a=f())&&(n.content=a.first),d(a=f())&&(n.prevButton=a.first),d(a=f())&&(n.nextButton=a.first),d(a=f())&&(n.inkbar=a.first),d(a=f())&&(n.tabs=a.first)}},hostVars:5,hostBindings:function(i,n){i&2&&(s("data-pc-name","tablist"),C("p-tablist",!0)("p-component",!0))},features:[p],ngContentSelectors:O,decls:9,vars:6,consts:[["content",""],["tabs",""],["inkbar",""],["prevButton",""],["nextButton",""],["type","button","pRipple","",1,"p-tablist-nav-button","p-tablist-prev-button"],[1,"p-tablist-content",3,"scroll","ngClass"],["role","tablist",1,"p-tablist-tab-list"],["role","presentation",1,"p-tablist-active-bar"],["type","button","pRipple","",1,"p-tablist-nav-button","p-tablist-next-button"],["type","button","pRipple","",1,"p-tablist-nav-button","p-tablist-prev-button",3,"click"],[4,"ngTemplateOutlet"],["type","button","pRipple","",1,"p-tablist-nav-button","p-tablist-next-button",3,"click"]],template:function(i,n){if(i&1){let a=Q();x(),h(0,Pt,4,4,"button",5),u(1,"div",6,0),F("scroll",function(W){return V(a),P(n.onScroll(W))}),u(3,"div",7,1),B(5),m(6,"span",8,2),v()(),h(8,At,4,4,"button",9)}i&2&&(w(n.showNavigators()&&n.isPrevButtonEnabled()?0:-1),T(),A("ngClass",rt(4,Et,n.scrollable())),T(5),s("data-pc-section","inkbar"),T(2),w(n.showNavigators()&&n.isNextButtonEnabled()?8:-1))},dependencies:[L,bt,pt,yt,_t,gt,Y,X],encapsulation:2,changeDetection:0})}return t})(),Kt=(()=>{class t extends ${value=S();disabled=y(!1,{transform:k});pcTabs=g(D(()=>q));pcTabList=g(D(()=>Ct));el=g(it);ripple=l(()=>this.config.ripple());id=l(()=>`${this.pcTabs.id()}_tab_${this.value()}`);ariaControls=l(()=>`${this.pcTabs.id()}_tabpanel_${this.value()}`);active=l(()=>J(this.pcTabs.value(),this.value()));tabindex=l(()=>this.active()?this.pcTabs.tabindex():-1);mutationObserver;onFocus(e){this.pcTabs.selectOnFocus()&&this.changeActiveValue()}onClick(e){this.changeActiveValue()}onKeyDown(e){switch(e.code){case"ArrowRight":this.onArrowRightKey(e);break;case"ArrowLeft":this.onArrowLeftKey(e);break;case"Home":this.onHomeKey(e);break;case"End":this.onEndKey(e);break;case"PageDown":this.onPageDownKey(e);break;case"PageUp":this.onPageUpKey(e);break;case"Enter":case"NumpadEnter":case"Space":this.onEnterKey(e);break;default:break}e.stopPropagation()}ngAfterViewInit(){super.ngAfterViewInit(),this.bindMutationObserver()}onArrowRightKey(e){let i=this.findNextTab(e.currentTarget);i?this.changeFocusedTab(e,i):this.onHomeKey(e),e.preventDefault()}onArrowLeftKey(e){let i=this.findPrevTab(e.currentTarget);i?this.changeFocusedTab(e,i):this.onEndKey(e),e.preventDefault()}onHomeKey(e){let i=this.findFirstTab();this.changeFocusedTab(e,i),e.preventDefault()}onEndKey(e){let i=this.findLastTab();this.changeFocusedTab(e,i),e.preventDefault()}onPageDownKey(e){this.scrollInView(this.findLastTab()),e.preventDefault()}onPageUpKey(e){this.scrollInView(this.findFirstTab()),e.preventDefault()}onEnterKey(e){this.changeActiveValue(),e.preventDefault()}findNextTab(e,i=!1){let n=i?e:e.nextElementSibling;return n?E(n,"data-p-disabled")||E(n,"data-pc-section")==="inkbar"?this.findNextTab(n):n:null}findPrevTab(e,i=!1){let n=i?e:e.previousElementSibling;return n?E(n,"data-p-disabled")||E(n,"data-pc-section")==="inkbar"?this.findPrevTab(n):n:null}findFirstTab(){return this.findNextTab(this.pcTabList?.tabs?.nativeElement?.firstElementChild,!0)}findLastTab(){return this.findPrevTab(this.pcTabList?.tabs?.nativeElement?.lastElementChild,!0)}changeActiveValue(){this.pcTabs.updateValue(this.value())}changeFocusedTab(e,i){ft(i),this.scrollInView(i)}scrollInView(e){e?.scrollIntoView?.({block:"nearest"})}bindMutationObserver(){K(this.platformId)&&(this.mutationObserver=new MutationObserver(e=>{e.forEach(()=>{this.active()&&this.pcTabList?.updateInkBar()})}),this.mutationObserver.observe(this.el.nativeElement,{childList:!0,characterData:!0,subtree:!0}))}unbindMutationObserver(){this.mutationObserver.disconnect()}ngOnDestroy(){this.mutationObserver&&this.unbindMutationObserver(),super.ngOnDestroy()}static \u0275fac=(()=>{let e;return function(n){return(e||(e=b(t)))(n||t)}})();static \u0275cmp=r({type:t,selectors:[["p-tab"]],hostVars:16,hostBindings:function(i,n){i&1&&F("focus",function(o){return n.onFocus(o)})("click",function(o){return n.onClick(o)})("keydown",function(o){return n.onKeyDown(o)}),i&2&&(s("data-pc-name","tab")("id",n.id())("aria-controls",n.ariaControls())("role","tab")("aria-selected",n.active())("data-p-disabled",n.disabled())("data-p-active",n.active())("tabindex",n.tabindex()),C("p-tab",!0)("p-tab-active",n.active())("p-disabled",n.disabled())("p-component",!0))},inputs:{value:[1,"value"],disabled:[1,"disabled"]},outputs:{value:"valueChange"},features:[ot([Y]),p],ngContentSelectors:O,decls:1,vars:0,template:function(i,n){i&1&&(x(),B(0))},dependencies:[L,X],encapsulation:2,changeDetection:0})}return t})(),jt=(()=>{class t extends ${pcTabs=g(D(()=>q));value=S(void 0);id=l(()=>`${this.pcTabs.id()}_tabpanel_${this.value()}`);ariaLabelledby=l(()=>`${this.pcTabs.id()}_tab_${this.value()}`);active=l(()=>J(this.pcTabs.value(),this.value()));static \u0275fac=(()=>{let e;return function(n){return(e||(e=b(t)))(n||t)}})();static \u0275cmp=r({type:t,selectors:[["p-tabpanel"]],hostVars:9,hostBindings:function(i,n){i&2&&(s("data-pc-name","tabpanel")("id",n.id())("role","tabpanel")("aria-labelledby",n.ariaLabelledby())("data-p-active",n.active()),C("p-tabpanel",!0)("p-component",!0))},inputs:{value:[1,"value"]},outputs:{value:"valueChange"},features:[p],ngContentSelectors:O,decls:1,vars:1,template:function(i,n){i&1&&(x(),h(0,Ht,1,0)),i&2&&w(n.active()?0:-1)},dependencies:[L],encapsulation:2,changeDetection:0})}return t})(),qt=(()=>{class t extends ${static \u0275fac=(()=>{let e;return function(n){return(e||(e=b(t)))(n||t)}})();static \u0275cmp=r({type:t,selectors:[["p-tabpanels"]],hostVars:6,hostBindings:function(i,n){i&2&&(s("data-pc-name","tabpanels")("role","presentation"),C("p-tabpanels",!0)("p-component",!0))},features:[p],ngContentSelectors:O,decls:1,vars:0,template:function(i,n){i&1&&(x(),B(0))},dependencies:[L],encapsulation:2,changeDetection:0})}return t})(),q=(()=>{class t extends ${value=S(void 0);scrollable=y(!1,{transform:k});lazy=y(!1,{transform:k});selectOnFocus=y(!1,{transform:k});showNavigators=y(!0,{transform:k});tabindex=y(0,{transform:lt});id=N(ht("pn_id_"));_componentStyle=g(Tt);updateValue(e){this.value.update(()=>e)}static \u0275fac=(()=>{let e;return function(n){return(e||(e=b(t)))(n||t)}})();static \u0275cmp=r({type:t,selectors:[["p-tabs"]],hostVars:8,hostBindings:function(i,n){i&2&&(s("data-pc-name","tabs")("id",n.id()),C("p-tabs",!0)("p-tabs-scrollable",n.scrollable())("p-component",!0))},inputs:{value:[1,"value"],scrollable:[1,"scrollable"],lazy:[1,"lazy"],selectOnFocus:[1,"selectOnFocus"],showNavigators:[1,"showNavigators"],tabindex:[1,"tabindex"]},outputs:{value:"valueChange"},features:[st([Tt]),p],ngContentSelectors:O,decls:1,vars:0,template:function(i,n){i&1&&(x(),B(0))},dependencies:[L],encapsulation:2,changeDetection:0})}return t})(),wt=(()=>{class t{static \u0275fac=function(i){return new(i||t)};static \u0275mod=at({type:t});static \u0275inj=nt({imports:[q,qt,jt,Ct,Kt]})}return t})();var xt=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=r({type:t,selectors:[["app-home"]],decls:0,vars:0,template:function(e,i){},dependencies:[wt],encapsulation:2})};export{xt as HomeComponent};

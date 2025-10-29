9f5fc97b34bd3f43d579721979168a0e6a32a4d0
neanchepersogno <neanchepersognorecords@gmail.com>
2025-10-29 23:41:11 +0100

    agggg

--- src/app/layouts/website/pages/moreismoreismore/moreismoreismore.component.ts
+++ src/app/layouts/website/pages/moreismoreismore/moreismoreismore.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-moreismoreismore',
@@ -7,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrl: './moreismoreismore.component.css'
 })
 export class MoreismoreismoreComponent {
+
+uriAssetsImages: string = environment.uriAssetsImages;
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+    const modal = document.getElementById('modal');
+    const modalImg= document.getElementById('modal-img');
+    const zoomableImages = document.querySelectorAll('.zoomable');
+    zoomableImages.forEach(img => {
+      img.addEventListener('click', () => {
+        modal!.style.display = 'flex';
+        modalImg!.setAttribute('src', img.getAttribute('src')!);
+        modalImg!.setAttribute('alt', img.getAttribute('alt')!);
+      });
+    });
+
+  }
+
+  // Functions
+  onCloseModal(): void{
+    const modal = document.getElementById('modal');
+    const modalImg= document.getElementById('modal-img');
+    modal!.style.display = 'none';
+    modalImg!.setAttribute('src', '');  
+    modalImg!.setAttribute('alt', '');
+  }
 
 }

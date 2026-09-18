19d44a83eecc7498318d6ec7ad74b98d9ca51c0c
neanchepersogno <neanchepersognorecords@gmail.com>
2026-09-18 22:25:21 +0200

    upddd

--- src/app/layouts/website/components/header/header.component.ts
+++ src/app/layouts/website/components/header/header.component.ts
@@ -1,20 +1,42 @@
 import { Component } from '@angular/core';
-import { RouterModule, Router } from '@angular/router';
+
+import {
+  RouterModule,
+  Router
+} from '@angular/router';
+
 import { environment } from '../../../../../environments/environment';
 
+
 @Component({
+
   selector: 'app-header',
+
   standalone: true,
+
   imports: [RouterModule],
+
   templateUrl: './header.component.html',
+
   styleUrl: './header.component.css'
+
 })
 export class HeaderComponent {
-  uriAssetsImages: string = environment.uriAssetsImages;
 
-  constructor(private router: Router) {}
+  uriAssetsImages: string =
+    environment.uriAssetsImages;
+
+
+  constructor(
+    private router: Router
+  ) {}
+
 
   isHome(): boolean {
-    return this.router.url === '/';
+
+    return this.router.url
+      .split('?')[0] === '/';
+
   }
+
 }
